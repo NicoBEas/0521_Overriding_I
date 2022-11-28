16690f3dbe57d0c069379df948406c340dd2c502
Biagio Mamone <BMAM179@more2learn.de>
2022-11-28 10:03:50 +0100

    Aktuell

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,9 @@ public class App {
     
     public static void main(String[] args) {
      
+        Child child = new Child();
+        output(child.sayHello());
+        output(child.sayHello("Hello from "));
     }
 
     public static void output(String outputStr) {
